8b88ac2632c0394705943493c45a9b626bb87311
marin <marin.bouthemy@gmail.com>
2022-10-12 19:40:09 +0800

    Init

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+"""
+Deploy the dashboard as an application.
+"""
+
+from explainerdashboard import ExplainerDashboard, ClassifierExplainer
+from flask import Flask
+
+app = Flask(__name__)
+
+
+explainer = ClassifierExplainer.from_file('explainer.joblib')
+db = ExplainerDashboard(explainer, server=app, url_base_pathname="/dashboard/")
+
+@app.route('/dashboard')
+def return_dashboard():
+    return db.app.index()
