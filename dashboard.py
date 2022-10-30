21f6ef5493541dfd1d5ef46c0860862efec883b9
msayyou <84775525+msayyou@users.noreply.github.com>
2022-10-30 12:34:55 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-explainer = ClassifierExplainer.from_file('explainer_1.joblib')
+explainer = ClassifierExplainer.from_file('explainer.joblib')
 db = ExplainerDashboard(explainer, server=app, url_base_pathname="/dashboard/")
 
 @app.route('/dashboard')
